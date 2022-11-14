dc19ff00ceda2d0bb92a477237b653d1ab6a993c
Charles Lee <charles.lee@safetyculture.io>
2022-11-15 09:36:54 +1100

    [INTG-2482] Renaming iAuditor->SafetyCulture

--- build.ps1
+++ build.ps1
@@ -7,18 +7,18 @@ New-Item -Path @rootFolder -Name "build" -ItemType "directory"
 Copy-Item "$rootFolder\*.png","$rootFolder\*.pqm","$rootFolder\*.resx" -Destination "$buildFolder"
 
 # Set the version inside Power Query file
-(Get-Content "$rootFolder\iAuditor.pq") `
+(Get-Content "$rootFolder\SafetyCulture.pq") `
     -replace '(\"sc-integration-version\"\s=\s).+\"', "`$1`"$ENV:CUR_VER`"" |
-  Out-File "$buildFolder\iAuditor.m"
+  Out-File "$buildFolder\SafetyCulture.m"
 
 # Compress files into an archive file
 $compress = @{
   Path = "$buildFolder\*"
   CompressionLevel = "Fastest"
-  DestinationPath = "$buildFolder\iAuditor.zip"
+  DestinationPath = "$buildFolder\SafetyCulture.zip"
 }
 Compress-Archive @compress
-Rename-Item -Path "$buildFolder\iAuditor.zip" -NewName "iAuditor.mez"
+Rename-Item -Path "$buildFolder\SafetyCulture.zip" -NewName "SafetyCulture.mez"
 
 # Generate certificate file from the base64 secret data
 $certFile = "$rootFolder\cert.pfx"
@@ -31,6 +31,6 @@ Expand-Archive "$pqxPackage" -DestinationPath "$rootFolder"
 $pqx = "$rootFolder\MakePQX_Release\MakePQX.exe"
 
 # Sign and verify the connector
-& $pqx pack -mz "$buildFolder\iAuditor.mez" -t "$buildFolder\iAuditorSigned.pqx"
-& $pqx sign "$buildFolder\iAuditorSigned.pqx" --certificate "$certFile" --password "$ENV:CERT_PASS"
-& $pqx verify "$buildFolder\iAuditorSigned.pqx"
+& $pqx pack -mz "$buildFolder\SafetyCulture.mez" -t "$buildFolder\SafetyCultureSigned.pqx"
+& $pqx sign "$buildFolder\SafetyCultureSigned.pqx" --certificate "$certFile" --password "$ENV:CERT_PASS"
+& $pqx verify "$buildFolder\SafetyCultureSigned.pqx"
